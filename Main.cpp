641abc0f413136d56a671cd4637d20aeece94ef5
vhparikh <42686619+vhparikh@users.noreply.github.com>
2021-01-27 15:37:44 -0800

    add edge

--- Main.cpp
+++ Main.cpp
@@ -35,6 +35,8 @@ int main() {
     list[i] = NULL;
   }
 
+  cout << "Welcome to Graph Creator!" << endl;
+  
   while (quit == false) {
 
     cin.get(input, 50);
@@ -45,14 +47,24 @@ int main() {
     }
 
     else if (strcmp(input, "add edge") == 0) {
-      //call edge
+      addEdge(list, adjacencyTable);
     }
     
     else if (strcmp(input, "print") == 0) {
+
       for (int i = 0; i < size; i++) {
 	if (list[i] != NULL) {
-	  cout << list[i]->name << endl;
+	  cout << list[i]->name << " ";
 	}
+      }
+      
+      cout << endl;
+      for (int i = 0; i < size; i++) {
+	for (int j = 0; j < size; j++) {
+	  cout << adjacencyTable[j][i] << " ";
+	}
+
+	cout << endl;
       }
     }
     
@@ -67,16 +79,58 @@ int main() {
 void addEdge(vertex** list, int** table) {
 
   char input[50];
-  int index = 0;
+  int data = 0;
+  int fIndex = 0;
+  int sIndex = 0;
   
   cout << "what is the first vertex of the edge" << endl;
   cin.get(input, 50);
   cin.get();
 
-  while (index < size && strcmp(list[index]->name, input) != 0) {
-    index++;
+  while (list[fIndex] != NULL && strcmp(list[fIndex]->name, input) != 0) {
+    fIndex++;
+  }
+
+  if (list[fIndex] == NULL || strcmp(list[fIndex]->name, input) != 0) {
+    cout << "the vertex is inexistent" << endl;
+    return;
   }
   
+  cout << "what is the second vertex of the edge" << endl;
+  cin.get(input, 50);
+  cin.get();
+
+  while (list[sIndex] != NULL && strcmp(list[sIndex]->name, input) != 0) {
+    sIndex++;
+  }
+
+  if (list[sIndex] == NULL || strcmp(list[sIndex]->name, input) != 0) {
+    cout << "the vertex is inexistent" << endl;
+    return;
+  }
+  
+  if (fIndex == sIndex) {
+    cout << "this is invalid can't have two of the same vertexes for edge" << endl;
+    return;
+  }
+  else {
+    cout << "enter the weight you would like for the edge" << endl;
+    cin >> data;
+    cin.get();
+
+    if (data >= 0) {
+
+      if (table[fIndex][sIndex] == -1) {
+	table[fIndex][sIndex] = data;
+      }
+      else {
+	cout << "an edge alread exists" << endl;
+      }
+    }
+    else {
+      cout << "invalid weight" << endl;
+    }
+  }
 }
 
 void addVertex(vertex** list) {
